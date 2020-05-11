b7be86211c0843f3483d1e680d0d0db9329044ba
jenseneducation-viktor-rizk <55272941+jenseneducation-viktor-rizk@users.noreply.github.com>
2020-05-11 09:35:18 +0100

    meerge

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 module.exports = {
   devServer: {
     https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem")
+      key: fs.readFileSync("./certs/192.168.10.238-key.pem"),
+      cert: fs.readFileSync("./certs/192.168.10.238.pem")
     }
   }
 };
